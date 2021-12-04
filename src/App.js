82ec2065840b136ed11b92f61f22eddba98913c7
hoiJuwon <withjuwon@gmail.com>
2021-12-04 17:43:07 +0900

    [feat] connect metamask with web3-react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
+import { Connect } from "./components";
+
+function getLibrary(provider) {
+  return new Web3(provider);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Connect />
+    </Web3ReactProvider>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { injected } from "./connectors";
+export { default as Connect } from "./Connect";
